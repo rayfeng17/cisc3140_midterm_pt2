9f699e9678d9bdddf378f8339866ed6e485a4587
Raymond Feng <54931410+rayfeng17@users.noreply.github.com>
2020-04-30 20:11:25 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -48,6 +48,7 @@ function formatPhoneText(value){
 }
 
 // return true if the string 'p' is a valid phone
+// digits only
 function validatePhone(p){
   var phoneInput = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
   var digits = p.replace(/\D/g, "");
